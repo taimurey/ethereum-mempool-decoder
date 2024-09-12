542a914b04fd413d790998830449c296121931c0
Taimoor <taimoorshafique541@gmail.com>
2024-09-12 12:32:55 +0500

    update: logger added, function mappings

--- src/mempool/decoder.rs
+++ src/mempool/decoder.rs
@@ -1,9 +1,10 @@
 use std::{error::Error, fs};
 
+use crate::types::logger::{log_decoded_input, token_to_string};
+
 use super::utils::UNIVERSAL_FUNCTION_MAPPING;
-use ethabi::{Contract, Error as EthabiError, Token};
-use ethers::types::{Address, Bytes};
-use log::{error, info};
+use ethabi::Contract;
+use ethers::types::Bytes;
 
 lazy_static::lazy_static! {
     pub static ref TARGET_POOL_ABI: String = fs::read_to_string("./Pool/WBTC-ETH.json")
@@ -17,23 +18,16 @@ lazy_static::lazy_static! {
 }
 
 pub async fn input_decoder(input: Bytes) -> Result<(), Box<dyn Error>> {
-    // Ensure the input is long enough to contain the function signature
     if input.len() < 4 {
         return Ok(());
     }
 
-    // Load the contract ABI
     let pool_contract = Contract::load(TARGET_POOL_ABI.as_bytes())?;
     let universal_contract = Contract::load(UNIVERSAL_ROUTER_ABI.as_bytes())?;
     let uniswap_v3_router_2 = Contract::load(UNISWAP_V3_ROUTER_V2.as_bytes())?;
 
-    // Extract the first 4 bytes of the input as the function signature
     let signature = &input[0..4];
 
-    println!("Signature: {:?}", signature);
-
-    // Identify the correct function using the signature
-    // If you have a mapping, use it here to find the function name
     let function_name = match UNIVERSAL_FUNCTION_MAPPING.get(signature) {
         Some(name) => name,
         None => return Ok(()),
@@ -46,15 +40,12 @@ pub async fn input_decoder(input: Bytes) -> Result<(), Box<dyn Error>> {
         _ => return Ok(()),
     };
 
-    // Extract the input data, skipping the first 4 bytes (signature)
     let data = &input[4..];
 
-    // Decode the input data according to the function's input types
     let tokens = function.decode_input(data)?;
 
     let types = function.inputs.clone();
 
-    // Check if the number of tokens matches the number of expected types
     if types.len() != tokens.len() {
         return Err("Mismatch between input types and decoded tokens.".into());
     }
@@ -62,80 +53,11 @@ pub async fn input_decoder(input: Bytes) -> Result<(), Box<dyn Error>> {
     let result = types
         .iter()
         .zip(tokens.iter())
-        .map(|(ty, to)| format!("{} {}", ty.kind, to))
+        .map(|(ty, to)| format!("{} {}", ty.kind, token_to_string(to)))
         .collect::<Vec<String>>()
         .join("\n");
 
-    info!("Decoded input: {:#?}", result);
+    log_decoded_input(function_name, &result);
 
     Ok(())
-}
-
-pub async fn decode_transaction_input(
-    input: &Bytes,
-    contract: &Contract,
-) -> Result<Option<(&'static str, Address)>, EthabiError> {
-    if input.0.len() < 4 {
-        return Err(EthabiError::InvalidData);
-    }
-
-    let signature = &input.0[0..4];
-
-    let function_name = UNIVERSAL_FUNCTION_MAPPING.get(signature);
-
-    match function_name {
-        Some(name) => {
-            let function = contract.function(name)?;
-            let tokens = function.decode_input(&input.0[4..])?;
-
-            match *name {
-                "swapExactETHForTokens"
-                | "swapExactETHForTokensSupportingFeeOnTransferTokens"
-                | "swapETHForExactTokens" => {
-                    info!("Method Name: {:?}", name);
-                    let mut address = None;
-                    if let Token::Array(path) = &tokens[1] {
-                        let mut path_iter = path.iter().skip(1);
-                        while let Some(token) = path_iter.next() {
-                            if let Token::Address(addr) = token {
-                                address = Some(*addr);
-                                println!(
-                                    "__________________________________________________________"
-                                );
-                                info!("Path: {:?}", addr);
-                                // let url = format!("https://www.dexview.com/eth/{:?}", addr);
-                                // if webbrowser::open(&url).is_ok() {
-                                //     info!("Opened transaction in web browser: {}", url);
-                                // } else {
-                                //     error!("Failed to open transaction in web browser: {}", url);
-                                // }
-
-                                let message = format!(
-                                    "0x{}",
-                                    addr.to_fixed_bytes()
-                                        .iter()
-                                        .map(|byte| format!("{:02x}", byte))
-                                        .collect::<String>()
-                                );
-
-                                println!("Message: {:#?}", message);
-                            } else {
-                                error!("Invalid token in path: {:?}", token);
-                            }
-                        }
-                    } else {
-                        error!("Invalid path in input: {:?}", tokens[1]);
-                    }
-                    let web3_address = if let Some(ethers_address) = address {
-                        ethers_address
-                    } else {
-                        return Err(EthabiError::InvalidData);
-                    };
-                    Ok(Some((*name, web3_address)))
-                }
-                _ => Ok(None),
-            }
-        }
-        None => Ok(None),
-    }
 }
---
+++ src/types/logger.rs
@@ -0,0 +1,77 @@
+use chrono::Local;
+use colored::Colorize;
+use ethabi::Token;
+
+pub fn token_to_string(token: &Token) -> String {
+    match token {
+        Token::Address(address) => format!("0x{}", hex::encode(address)),
+        Token::Uint(uint) => uint.to_string(),
+        Token::Int(int) => int.to_string(),
+        Token::Bool(b) => b.to_string(),
+        Token::String(s) => s.clone(),
+        Token::Bytes(bytes) => format!("0x{}", hex::encode(bytes)),
+        Token::FixedBytes(bytes) => format!("0x{}", hex::encode(bytes)),
+        Token::Array(arr) => format!(
+            "[{}]",
+            arr.iter()
+                .map(token_to_string)
+                .collect::<Vec<_>>()
+                .join(", ")
+        ),
+        Token::FixedArray(arr) => format!(
+            "[{}]",
+            arr.iter()
+                .map(token_to_string)
+                .collect::<Vec<_>>()
+                .join(", ")
+        ),
+        Token::Tuple(tuple) => format!(
+            "({})",
+            tuple
+                .iter()
+                .map(token_to_string)
+                .collect::<Vec<_>>()
+                .join(", ")
+        ),
+    }
+}
+
+pub fn log_decoded_input(function_name: &str, input_data: &str) {
+    let timestamp = Local::now().format("%H:%M:%S%.3f").to_string();
+    let header = format!("{} ⮞ Decoded input for {}:", timestamp, function_name).bright_blue();
+
+    println!("\n{}", header);
+
+    if let Some((param_type, value)) = input_data.split_once(' ') {
+        let formatted_type = format!("{}", param_type).yellow();
+        println!("  {}", formatted_type);
+
+        if param_type.starts_with("(") {
+            // For tuples, split the content and display each item on a new line
+            let items = value
+                .trim_start_matches('(')
+                .trim_end_matches(')')
+                .split(',');
+            let types = param_type
+                .trim_start_matches('(')
+                .trim_end_matches(')')
+                .split(',');
+
+            for (item_type, item_value) in types.zip(items) {
+                let formatted_item_type = format!("    {:<15}", item_type.trim()).yellow();
+                let formatted_item_value = item_value.trim().bright_green();
+                println!("  {} {}", formatted_item_type, formatted_item_value);
+            }
+        } else if param_type.starts_with("bytes") {
+            let formatted_value = format!("0x{}...", &value[2..8]).bright_green();
+            println!("    {}", formatted_value);
+        } else {
+            let formatted_value = value.bright_green();
+            println!("    {}", formatted_value);
+        }
+    } else {
+        println!("  {}", input_data.bright_green());
+    }
+
+    println!();
+}
